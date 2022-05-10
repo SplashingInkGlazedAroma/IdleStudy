42477c2de8c8b1930e4208c31f1fd6ade1bc7e06
joker520 <17805050929@163.com>
2022-05-10 17:02:19 +0800

    commit

---
+++ src/main/java/com/xtl/other/Hello.java
@@ -0,0 +1,13 @@
+package com.xtl.other;
+
+/**
+ * @author 31925
+ * @className Hello
+ * @description TODO
+ * @date 2022/5/10 17:00
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
